52db6798819e938448e945c3514ae54a04d83ae7
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-07-15 16:57:47 +0300

    Configuring the cell display

--- FreeGames/ViewControllers/MainViewController.swift
+++ FreeGames/ViewControllers/MainViewController.swift
@@ -7,20 +7,31 @@
 
 import UIKit
 
+enum URL: String {
+    case mainURL = "https://www.freetogame.com/api/"
+}
+
 class MainViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        0
+        1
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "games", for: indexPath) as? GamesAllCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
     }
 
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 36, height: 200)
+    }
 }
 
