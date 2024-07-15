fe8e2f6ca0389df54d2961ebd7562a209a883db0
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-07-15 16:28:19 +0300

    Rename Main Controller. Add UiCollectionViewSell. Change in Storyboard

---
+++ FreeGames/ViewControllers/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  FreeGames
+//
+//  Created by Denis Bokov on 15.07.2024.
+//
+
+import UIKit
+
+class MainViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+
+}
+
---
+++ FreeGames/Views/GamesAllCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GamesAllCollectionViewCell.swift
+//  FreeGames
+//
+//  Created by Denis Bokov on 15.07.2024.
+//
+
+import UIKit
+
+class GamesAllCollectionViewCell: UICollectionViewCell {
+    
+}
